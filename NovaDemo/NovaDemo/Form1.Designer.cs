da767209996969d02de124d2069ec344878f15f3
Anthony DuPont <tony@nebland.com>
2016-11-21 13:55:52 -0500

    add code to populate DataGridView with request data

--- NovaDemo/NovaDemo/Form1.Designer.cs
+++ NovaDemo/NovaDemo/Form1.Designer.cs
@@ -28,61 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.EventID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.StartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+			this.DGEvent = new System.Windows.Forms.DataGridView();
+			this.EventID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.StartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.DGEvent)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// DGEvent
+			// 
+			this.DGEvent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.DGEvent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.EventID,
             this.StartTime,
             this.Duration});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(493, 320);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // EventID
-            // 
-            this.EventID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.EventID.HeaderText = "Event ID";
-            this.EventID.Name = "EventID";
-            this.EventID.ReadOnly = true;
-            // 
-            // StartTime
-            // 
-            this.StartTime.HeaderText = "Start Time";
-            this.StartTime.Name = "StartTime";
-            this.StartTime.ReadOnly = true;
-            // 
-            // Duration
-            // 
-            this.Duration.HeaderText = "Duration";
-            this.Duration.Name = "Duration";
-            this.Duration.ReadOnly = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 320);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+			this.DGEvent.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.DGEvent.Location = new System.Drawing.Point(0, 0);
+			this.DGEvent.Name = "DGEvent";
+			this.DGEvent.Size = new System.Drawing.Size(493, 320);
+			this.DGEvent.TabIndex = 0;
+			// 
+			// EventID
+			// 
+			this.EventID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.EventID.HeaderText = "Event ID";
+			this.EventID.Name = "EventID";
+			this.EventID.ReadOnly = true;
+			// 
+			// StartTime
+			// 
+			this.StartTime.HeaderText = "Start Time";
+			this.StartTime.Name = "StartTime";
+			this.StartTime.ReadOnly = true;
+			// 
+			// Duration
+			// 
+			this.Duration.HeaderText = "Duration";
+			this.Duration.Name = "Duration";
+			this.Duration.ReadOnly = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(493, 320);
+			this.Controls.Add(this.DGEvent);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.DGEvent)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DGEvent;
         private System.Windows.Forms.DataGridViewTextBoxColumn EventID;
         private System.Windows.Forms.DataGridViewTextBoxColumn StartTime;
         private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
--- NovaDemo/NovaDemo/Form1.cs
+++ NovaDemo/NovaDemo/Form1.cs
@@ -7,14 +7,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
 
 namespace NovaDemo
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		private HttpListener m_listener = new HttpListener();
+		Thread m_listenerThread = null;
+		bool m_isRequestHandlerExited = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+			// m_listener.Prefixes.Add("http://localhost:8383/newevent/");
+			// m_listener.Prefixes.Add("http://127.0.0.1:8383/newevent/");
+			m_listener.Prefixes.Add("http://141.219.240.248:7999/newevent/");
+			// m_listener.Prefixes.Add("http://10.0.2.15:83/newevent/");
+		}
+
+
+		private void RequestHandler()
+		{
+			Console.WriteLine("requestHandler called");
+			while (m_listener.IsListening)
+			{
+				IAsyncResult context = m_listener.BeginGetContext(new AsyncCallback(ListenerCallback), m_listener);
+				context.AsyncWaitHandle.WaitOne();
+			}
+
+			m_isRequestHandlerExited = true;
+		}
+
+
+		private void ListenerCallback(IAsyncResult ar)
+		{
+			Console.WriteLine("ListenerCallback");
+			// TODO: what is the proper check here
+			// TODO: add try catch
+			// not sure what the proper check is here but this code throws when the 
+			// program is shut down if this check isn't here
+			if (m_listener.IsListening)
+			{
+				HttpListener listener = ar.AsyncState as HttpListener;
+
+				HttpListenerContext context = listener.EndGetContext(ar);
+
+				HttpListenerRequest request = context.Request;
+
+				long dataLength = request.ContentLength64;
+
+				string text;
+				using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+				{
+					text = reader.ReadToEnd();
+				}
+
+				// DGEvent.Rows.Add(text, "aoeu", "aoeu");
+				DGEvent.BeginInvoke((MethodInvoker)delegate () { DGEvent.Rows.Add(text, "aoeu", "aoeu"); });
+
+				Console.WriteLine("size of buffer " + text);
+			}
+		}
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				Console.WriteLine("Form1_Load");
+				// start the listener first... 
+				m_listener.Start();
+
+				// ...because the thread will exit when the listener is no longer listening
+				m_listenerThread = new Thread(new ThreadStart(RequestHandler));
+
+				m_listenerThread.Start();
+			}
+			catch (Exception exception)
+			{
+				Console.Out.WriteLine("Form1_Load exception:\n" + exception.Message);
+			}
+		}
+
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			try
+			{
+				// this will also cause the listener thread to stop
+				m_listener.Stop();
+
+				// TODO: determine if this needs to be done and if so, what way it should be done
+				// attempting to wait while pending requests are being processed
+				while (!m_isRequestHandlerExited) { }
+
+				m_listener.Close();
+
+				m_listenerThread.Join();
+			}
+			catch (Exception exception)
+			{
+				Console.Out.WriteLine("From1_FromClosed exception:\n" + exception.Message);
+			}
+		}
+	}
 }
